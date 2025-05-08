291359acdf248b8b01857660889b6e965fe386a7
gregluo <greg.luo@outlook.com>
2025-05-08 20:56:10 +1000

    fix cors issue

--- app/api/counter/route.ts
+++ app/api/counter/route.ts
@@ -1,13 +1,25 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-
+import { middleware } from "../middleware";
 
 export async function POST(request: NextRequest) {
+  const headers = middleware(request);
+  if (request.method === "OPTIONS") {
+    // Handle preflight OPTIONS request
+    return new NextResponse(null, {
+      status: 204,
+      headers,
+    });
+  }
+
   const body: { amount: number } = await request.json();
   const { amount = 1 } = body;
 
   // simulate IO latency
   await new Promise((resolve) => setTimeout(resolve, 500));
 
-  return NextResponse.json({ data: amount });
+  return new NextResponse(JSON.stringify({ data: amount }), {
+    status: 200,
+    headers,
+  });
 }
